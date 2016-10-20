4d4fc0a1aadb6ffe042c4d4a99347d085efcfc82
Orivej Desh <orivej@gmx.fr>
2016-10-20 08:22:54 +0000

    Add -log-dir option

--- main.go
+++ main.go
@@ -6,8 +6,12 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net"
+	"os"
+	"path/filepath"
 	"strings"
+	"time"
 
 	"github.com/orivej/e"
 	"github.com/pkg/errors"
@@ -19,6 +23,8 @@ type ReadResult struct {
 	buf []byte
 }
 
+type Log func(side, data string)
+
 const (
 	bufSize         = 4000
 	clientTLSMarker = "<starttls"
@@ -28,6 +34,7 @@ const (
 var (
 	flListenPort = flag.Int("port", 5222, "listen port")
 	flServer     = flag.String("server", "<server:port>", "target server:port")
+	flLogDir     = flag.String("log-dir", "", "directory for session logs")
 
 	flKeyPath  = flag.String("key", "", "path to TLS certificate key")
 	flCertPath = flag.String("cert", "", "path to TLS certificate")
@@ -49,25 +56,63 @@ func main() {
 	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", *flListenPort))
 	e.Exit(err)
 
-	for idx := 1; true; idx++ {
+	for idx := nextIdx(); true; idx++ {
+		idx := idx
 		client, err := listener.Accept()
 		e.Exit(err)
-		go func(idx int) {
+
+		var f *os.File
+		if *flLogDir != "" {
+			path := filepath.Join(*flLogDir, logName(idx))
+			flags := os.O_WRONLY | os.O_CREATE | os.O_EXCL
+			f, err = os.OpenFile(path, flags, 0666)
+			e.Exit(err)
+		}
+		pr := func(side, data string) {
+			ts := time.Now().Format(time.RFC3339)
+			fmt.Printf("%04d %s %s %s\n", idx, ts, side, data)
+			if f != nil {
+				fmt.Fprintf(f, "%s %s %s\n", ts, side, data)
+			}
+		}
+
+		go func() {
 			defer e.CloseOrPrint(client)
-			err := serve(client, idx, certificate)
-			fmt.Printf("X%d: %s\n", idx, err)
-		}(idx)
+			err := serve(client, pr, certificate)
+			pr("X", err.Error())
+		}()
 	}
 }
 
-func serve(client net.Conn, idx int, certificate tls.Certificate) error {
+func nextIdx() int {
+	if *flLogDir == "" {
+		return 1
+	}
+
+	files, err := ioutil.ReadDir(*flLogDir)
+	e.Exit(err)
+
+	idx := 1
+	for _, file := range files {
+		if file.Name() == logName(idx) {
+			idx++
+		}
+	}
+	return idx
+}
+
+func logName(idx int) string {
+	return fmt.Sprintf("%04d.log", idx)
+}
+
+func serve(client net.Conn, pr Log, certificate tls.Certificate) error {
 	server, err := net.Dial("tcp", *flServer)
 	if err != nil {
 		return errors.Wrap(err, "failed to connect to server")
 	}
 	defer e.CloseOrPrint(server)
 
-	err = proxy(client, server, idx, true)
+	err = proxy(client, server, pr, true)
 	if err != nil {
 		return errors.Wrap(err, "session terminated")
 	}
@@ -78,12 +123,12 @@ func serve(client net.Conn, idx int, certificate tls.Certificate) error {
 	tlsServer := tls.Client(server, &tls.Config{
 		InsecureSkipVerify: true,
 	})
-	fmt.Printf("X%d: eavesdropping TLS\n", idx)
-	err = proxy(tlsClient, tlsServer, idx, false)
+	pr("X", "eavesdropping TLS")
+	err = proxy(tlsClient, tlsServer, pr, false)
 	return errors.Wrap(err, "TLS session terminated")
 }
 
-func proxy(client, server io.ReadWriter, idx int, untilTLS bool) error {
+func proxy(client, server io.ReadWriter, pr Log, untilTLS bool) error {
 	clientNext, clientResults := startReader(client)
 	serverNext, serverResults := startReader(server)
 
@@ -103,7 +148,7 @@ func proxy(client, server io.ReadWriter, idx int, untilTLS bool) error {
 			if replace {
 				buf = bytes.Replace(buf, rLocal, rRemote, -1)
 			}
-			fmt.Printf("C%d|%s|\n", idx, escape(string(buf)))
+			pr("?", escape(string(buf)))
 
 			_, err = server.Write(buf)
 			if err != nil {
@@ -111,7 +156,7 @@ func proxy(client, server io.ReadWriter, idx int, untilTLS bool) error {
 			}
 
 			if untilTLS && bytes.Contains(buf, []byte(clientTLSMarker)) {
-				fmt.Printf("X%d: client goes TLS\n", idx)
+				pr("X", "client requests TLS")
 				clientNext <- false
 				continue
 			}
@@ -125,7 +170,7 @@ func proxy(client, server io.ReadWriter, idx int, untilTLS bool) error {
 			}
 
 			buf := result.buf[:result.n]
-			fmt.Printf("S%d|%s|\n", idx, escape(string(buf)))
+			pr("!", escape(string(buf)))
 			if replace {
 				buf = bytes.Replace(buf, rRemote, rLocal, -1)
 			}
@@ -136,7 +181,7 @@ func proxy(client, server io.ReadWriter, idx int, untilTLS bool) error {
 			}
 
 			if untilTLS && bytes.Contains(buf, []byte(serverTLSMarker)) {
-				fmt.Printf("X%d: server goes TLS\n", idx)
+				pr("X", "server approves TLS")
 				serverNext <- false
 				return nil
 			}
@@ -162,8 +207,12 @@ func startReader(reader io.Reader) (chan<- bool, chan ReadResult) {
 	return readNext, results
 }
 
+var escaper = strings.NewReplacer(
+	"\\", "\\\\",
+	"\n", "\\n",
+	"\r", "\\r",
+)
+
 func escape(s string) string {
-	s = strings.Replace(s, "\\", "\\\\", -1)
-	s = strings.Replace(s, "\n", "\\n", -1)
-	return s
+	return escaper.Replace(s)
 }
